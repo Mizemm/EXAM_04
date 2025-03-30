cf9024752a170a51ff8ceab738640be6486f9b3e
Mohamed-Amine Izem <mizem@c2r1p9.1337.ma>
2025-03-30 04:50:19 +0000

    f

--- level_1/ft_popen/ft_popen.c
+++ level_1/ft_popen/ft_popen.c
@@ -1,58 +1,44 @@
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include <sys/wait.h>
 
-int ft_popen(const char *file, char *const argv[], char type)
+int ft_popen(const char *file, const char *argv[], char type)
 {
-    pid_t pid;
-    int fd[2];
+    if (!file || !argv || (type != 'r' && type != 'w'))
+        return -1;
 
-    if (!file || !argv || (type != 'r' && type != 'w') || pipe(fd) == -1)
+    int fd[2];
+    if (pipe(fd) == -1)
         return -1;
-    pid = fork();
-    if (pid < 0)
-    {
+
+    pid_t pid = fork();
+    if (pid == -1) {
         close(fd[0]);
         close(fd[1]);
         return -1;
     }
-    if (pid == 0)
-    {
-        if (type == 'r')
-        {
-            close(fd[0]);
-            if (dup2(fd[1], 1) == -1)
-                exit(-1);
-        }
-        else if (type == 'w')
-        {
+    if (pid == 0) {  // Child process
+        if (type == 'r') {
+            close(fd[0]);          // Close unused read end
+            dup2(fd[1], STDOUT_FILENO);
             close(fd[1]);
-            if (dup2(fd[0], 0) == -1)
-                exit(-1);
+        } else {  // type == 'w'
+            close(fd[1]);          // Close unused write end
+            dup2(fd[0], STDIN_FILENO);
+            close(fd[0]);
         }
-        close(fd[0]);
-        close(fd[1]);
-        if (execvp(file, argv) == -1)
-            exit(-1);
+        execvp(file, (char *const *)argv);
+        exit(-1);  // Only reached if execvp fails
     }
-    int tmp_fd;
-    if (type == 'r') 
-    {
-        close(fd[1]);
-        tmp_fd = fd[0];
-        close (fd[0]);
-        return tmp_fd;
+    if (type == 'r') {
+        close(fd[1]);  // Close write end in parent
+        return fd[0];  // Return read end
+    } else {  // type == 'w'
+        close(fd[0]);  // Close read end in parent
+        return fd[1];  // Return write end
     }
-    else if (type == 'w') 
-    {
-        close(fd[0]);
-        tmp_fd = fd[1];
-        close(fd[1]);
-        return tmp_fd;
-    }
-    return 0;
 }
+
 int main() 
 {
     printf("fd : %d\n", ft_popen("ls", (char *const[]){"ls", NULL}, 'r'));
--- level_1/sandbox/sandbox.c
+++ level_1/sandbox/sandbox.c
@@ -1,56 +1,86 @@
 #include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
 
-int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
+void do_nothing(int sig)
 {
-    pid_t pid;
-    int status;
-
-    pid = fork();
-    if (pid < 0)
-        return -1;
-    if (pid == 0)
-    {
-        alarm(timeout);
-        f();
-        exit(0);
-    }
-    if (waitpid(pid, &status, 0) == -1)
-        return -1;
-    if (WIFEXITED(status))
-    {
-        if (WEXITSTATUS(status) == 0)
-        {
-            if (verbose)
-                printf("Nice function!\n");
-            return 1;
-        }
-        if (verbose)
-            printf("Bad function: exited with code %d\n", WEXITSTATUS(status));
-        return 0;
-    }
-    if (WIFSIGNALED(status))
-    {
-        if (verbose)
-            printf("Bad function: %s\n", strsignal(WTERMSIG(status)));
-        return 0;
-    }
-    return 0;
+	(void)sig;
 }
 
+int	sandbox(void (*f)(void), unsigned int timeout, bool verbose)
+{
+	pid_t pid = fork();
+	if (pid == -1)
+		return (-1);
+
+	if (pid == 0)
+	{
+		alarm(timeout); 
+		f();
+		exit(0);
+	}
+
+	struct sigaction sa;
+	sa.sa_handler = do_nothing;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGALRM, &sa, NULL);
+	int st;
+	pid_t r = waitpid(pid, &st, 0);
+	
+	if (r == -1)
+	{
+		if (errno == EINTR)
+		{
+			kill(pid, SIGKILL);
+			waitpid(pid, NULL, 0);
+			if (verbose)
+				printf("Bad function: timed out after %d seconds\n", timeout);
+			return (0);
+		}
+		return (-1);
+	}
+	if (WIFEXITED(st))
+	{
+		int ex_code = WEXITSTATUS(st);
+		if (ex_code == 0)
+		{
+			if (verbose)
+				printf("Nice function!\n");
+			return (1);
+		}
+		if (verbose)
+			printf("Bad function: exited with code %d\n", ex_code);
+		return (0);
+	}
+	if (WIFSIGNALED(st))
+	{
+		if (WTERMSIG(st) == SIGALRM)
+		{
+			if (verbose)
+				printf("Bad function: timed out after %d seconds\n", timeout);
+			return (0);
+		}
+		if (verbose)
+			printf("Bad function: %s\n", strsignal(WTERMSIG(st)));
+		return (0);
+	}
+	return (-1);
+}
 
 void fun1() {
     printf("alo\n");
-    sleep(1);
-    printf("alo2\n");
-    sleep(1);
-    printf("alo3\n");
-    sleep(1);
+    sleep(2);
+    // printf("alo2\n");
+    // sleep(1);
+//     printf("alo3\n");
+//     sleep(1);
 }
 
 void fun2() {
@@ -64,9 +94,9 @@ void fun3() {
 
 int main()
 {
-    sandbox(fun1, 2, 1); // Test function that segfaults
-    sandbox(fun2, 5, 1);  // Test function that segfaults
-    sandbox(fun3, 5, 1);  // Test function that segfaults
+    sandbox(fun1, 1, 1);
+    // sandbox(fun2, 5, 1);
+    // sandbox(fun3, 5, 1);
     while(1)
     {
 
